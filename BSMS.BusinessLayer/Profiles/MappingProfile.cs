f536165b44d24b7137a4f822a31b36168058d125
michaela <michaellarif04@gmail.com>
2024-12-27 17:55:48 +0200

    removed the unnecessary mapping between the cities.country_id and countries.country_id

--- BSMS.BusinessLayer/Profiles/MappingProfile.cs
+++ BSMS.BusinessLayer/Profiles/MappingProfile.cs
@@ -9,7 +9,6 @@ namespace BSMS.BusinessLayer.Profiles
         public MappingProfile()
         {
             CreateMap<City, CityDto>()
-                .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Country.CountryName))
                 .ReverseMap();
 
 
