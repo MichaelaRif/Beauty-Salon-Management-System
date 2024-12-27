1ac21ba0db84f7b7e118cb07c72202c6e8c6cc4e
michaela <michaellarif04@gmail.com>
2024-12-28 00:26:11 +0200

    implemented GetByIdAsync and GetAllAsync

--- BSMS.PostgreSQL/Repositories/AddressRepository.cs
+++ BSMS.PostgreSQL/Repositories/AddressRepository.cs
@@ -1,10 +1,28 @@
 using BSMS.Data.Common.Interfaces;
 using BSMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BSMS.PostgreSQL.Repositories
 {
     public class AddressRepository : BaseRepository<Address>, IAddressRepository
     {
         public AddressRepository(BSMSDbContext context) : base(context) { }
+
+        public override async Task<Address?> GetByIdAsync(int id)
+        {
+            return await _context.Addresses
+                .Where(address => address.AddressId == id)
+                .Include(address => address.AddressCity)
+                    .ThenInclude(city => city.Country)
+                .FirstOrDefaultAsync(x => x.AddressId == id);
+        }
+
+        public override async Task<IEnumerable<Address>> GetAllAsync()
+        {
+            return await _context.Addresses
+                .Include(address => address.AddressCity)
+                    .ThenInclude(city => city.Country)
+                .ToListAsync();
+        }
     }
 }
