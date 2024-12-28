fc8e3b95354c491f1e2ddc5b889e955ae34b7802
michaela <michaellarif04@gmail.com>
2024-12-29 00:53:08 +0200

    refactored IRepository to IRepositoryCommand and IRepositoryQuery

---
+++ BSMS.Data.Common/Interfaces/Common/IRepositoryQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace BSMS.Data.Common.Interfaces.Common
+{
+    public interface IRepositoryQuery<T> where T : class
+    {
+        Task<IEnumerable<T>?> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+    }
+
+}
