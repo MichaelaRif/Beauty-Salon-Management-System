f2aa3d944886069c758365504b6a76443bd51276
michaela <michaellarif04@gmail.com>
2025-01-11 23:18:32 +0200

    added CustomerInfoDto and its necessary mapping

---
+++ BSMS.BusinessLayer/DTOs/CustomerInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BSMS.BusinessLayer.DTOs
+{
+    public class CustomerInfoDto
+    {
+        public CustomerDto Customer { get; set; } = null!;
+        public AddressDto Address { get; set; } = null!;
+    }
+}
--- BSMS.BusinessLayer/Profiles/MappingProfile.cs
+++ BSMS.BusinessLayer/Profiles/MappingProfile.cs
@@ -52,6 +52,20 @@ namespace BSMS.BusinessLayer.Profiles
                 .ForMember(dest => dest.AddressNotes, opt => opt.MapFrom(src => src.AddressNotes))
                 .ReverseMap();
 
+            CreateMap<CustomerInfoDto, UserInfo>()
+                .ForMember(dest => dest.PronounName, opt => opt.MapFrom(src => src.Customer.PronounName))
+                .ForMember(dest => dest.DateOfBirth, opt => opt.MapFrom(src => src.Customer.CustomerDob))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Customer.CustomerPn))
+                .ForMember(dest => dest.GivenName, opt => opt.MapFrom(src => src.Customer.CustomerFn))
+                .ForMember(dest => dest.FamilyName, opt => opt.MapFrom(src => src.Customer.CustomerLn))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Customer.CustomerEmail))
+                .ForMember(dest => dest.CityName, opt => opt.MapFrom(src => src.Address.AddressCity))
+                .ForMember(dest => dest.AddressStreet, opt => opt.MapFrom(src => src.Address.AddressStreet))
+                .ForMember(dest => dest.AddressBuilding, opt => opt.MapFrom(src => src.Address.AddressBuilding))
+                .ForMember(dest => dest.AddressFloor, opt => opt.MapFrom(src => src.Address.AddressFloor))
+                .ForMember(dest => dest.AddressNotes, opt => opt.MapFrom(src => src.Address.AddressNotes))
+                .ReverseMap();
+
             CreateMap<Pronoun, PronounDto>()
                 .ReverseMap();
 
