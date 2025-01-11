adb027bec8c14d379c124b1fa1c71cd2aedaa088
michaela <michaellarif04@gmail.com>
2025-01-11 21:36:56 +0200

    updated the fields to correspond to the user profile in keycloak

--- BSMS.BusinessLayer/DTOs/CustomerDto.cs
+++ BSMS.BusinessLayer/DTOs/CustomerDto.cs
@@ -7,12 +7,10 @@
         public string? CustomerEmail { get; set; }
         public string? CustomerPn { get; set; }
         public DateOnly CustomerDob { get; set; }
-        public PronounDto CustomerPronoun { get; set; } = null!;
-        public bool Promotions { get; set; }
-        public string? CustomerPfp { get; set; }
         public bool IsGoogle { get; set; }
         public bool IsApple { get; set; }
         public bool Is2fa { get; set; }
+        public string PronounName { get; set; } = null!;
 
     }
 }
