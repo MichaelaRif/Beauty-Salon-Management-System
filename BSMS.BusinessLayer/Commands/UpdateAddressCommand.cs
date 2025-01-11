fbf6abab5769f59bb2f445864374b63cab36bf23
michaela <michaellarif04@gmail.com>
2025-01-11 21:44:46 +0200

    modified the type of AddressFloor

--- BSMS.BusinessLayer/Commands/UpdateAddressCommand.cs
+++ BSMS.BusinessLayer/Commands/UpdateAddressCommand.cs
@@ -7,7 +7,7 @@ namespace BSMS.BusinessLayer.Commands
         public int AddressId { get; set; }
         public string AddressStreet { get; set; } = null!;
         public string AddressBuilding { get; set; } = null!;
-        public string AddressFloor { get; set; } = null!;
+        public int AddressFloor { get; set; }
         public string? AddressNotes { get; set; }
         public int City { get; set; }
     }
