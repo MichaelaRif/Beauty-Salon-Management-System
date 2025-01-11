d0a5a75fbd8be086f01de91b2c25c93da51f92e9
michaela <michaellarif04@gmail.com>
2025-01-12 01:09:36 +0200

    implemented PreferencesController

---
+++ BSMS.WebAPI/Controllers/PreferencesController.cs
@@ -0,0 +1,26 @@
+﻿using BSMS.BusinessLayer.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSMS.WebAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/customers/preferences")]
+    public class PreferencesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PreferencesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPreferences()
+        {
+            return Ok(await _mediator.Send(new GetAllPreferencesQuery {}));
+        }
+    }
+}
