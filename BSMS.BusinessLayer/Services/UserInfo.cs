d9746ea36b5d34dcd004923beff858d5dbef0d2c
michaela <michaellarif04@gmail.com>
2025-01-11 20:57:17 +0200

    updated UserInfo to correspond to the user profile in keycloak

--- BSMS.BusinessLayer/Services/UserInfo.cs
+++ BSMS.BusinessLayer/Services/UserInfo.cs
@@ -7,11 +7,8 @@ namespace BSMS.BusinessLayer.Services
         [JsonProperty("sub")]
         public string Sub { get; set; } = null!;
 
-        [JsonProperty("promotions")]
-        public bool Promotions { get; set; }
-
         [JsonProperty("pronoun")]
-        public string Pronoun { get; set; } = null!;
+        public string PronounName { get; set; } = null!;
 
         [JsonProperty("date_of_birth")]
         public DateOnly DateOfBirth { get; set; }
@@ -27,5 +24,20 @@ namespace BSMS.BusinessLayer.Services
 
         [JsonProperty("email")]
         public string Email { get; set; } = null!;
+
+        [JsonProperty("city")]
+        public string CityName { get; set; } = null!;
+
+        [JsonProperty("address_street")]
+        public string AddressStreet { get; set; } = null!;
+
+        [JsonProperty("address_building")]
+        public string AddressBuilding { get; set; } = null!;
+
+        [JsonProperty("address_floor")]
+        public int AddressFloor { get; set; }
+
+        [JsonProperty("address_notes")]
+        public string? AddressNotes { get; set; }
     }
 }
