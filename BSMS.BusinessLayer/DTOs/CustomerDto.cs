2d45f4af498314217134bd2a533ee47bd0bbaceb
michaela <michaellarif04@gmail.com>
2025-01-12 14:49:38 +0200

    removed unnecessary fields

--- BSMS.BusinessLayer/DTOs/CustomerDto.cs
+++ BSMS.BusinessLayer/DTOs/CustomerDto.cs
@@ -7,9 +7,6 @@
         public string? CustomerEmail { get; set; }
         public string? CustomerPn { get; set; }
         public DateOnly CustomerDob { get; set; }
-        public bool IsGoogle { get; set; }
-        public bool IsApple { get; set; }
-        public bool Is2fa { get; set; }
         public string PronounName { get; set; } = null!;
 
     }
